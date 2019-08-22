f93f6fd277e0ca9762c5e8a7343cc73f2182e99f
Dan Shields <d.shieldsuk@gmail.com>
2019-08-22 01:08:11 +0100

    chore: bin off typescript

---
+++ src/router.js
@@ -0,0 +1,47 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import Events from './views/Events.vue';
+import Socials from './views/Socials.vue';
+import Committee from './views/Committee.vue';
+import Contact from './views/Contact.vue';
+import About from './views/About.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/socials',
+      name: 'Events',
+      component: Events
+    },
+    {
+      path: '/socials',
+      name: 'Socials',
+      component: Socials
+    },
+    {
+      path: '/committee',
+      name: 'Committee',
+      component: Committee
+    },
+    {
+      path: '/contact',
+      name: 'Contact',
+      component: Contact
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
+    }
+  ]
+});
