bb0198bdf05cbf00f136a44001fa3a62fa9effcc
Dan Shields <d.shieldsuk@gmail.com>
2019-09-01 01:53:05 +0100

    feat: add committee view (awaiting all images)

---
+++ src/components/CommitteeMember.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="committee-member" :class="{right: index % 2}">
+    <img :src="require('../assets/committee/' + member.img + '.jpg')" class="image" :class="{right: index % 2}">
+    <div class="text" :class="{right: index % 2}">
+      <h1 class="name">{{member.name}}</h1>
+      <h2 class="title">{{member.title}}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'side',
+    'member',
+    'index'
+  ]
+};
+</script>
+
+<style lang="scss" scoped>
+.committee-member {
+  padding: 10px;
+  width: 100%;
+  height: 250px;
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  position: relative;
+  align-items: center;
+
+  > * {
+    position: relative;
+  }
+
+  &.right {
+    justify-content: right;
+  }
+
+  .image {
+    height: 100%;
+  }
+  .text {
+    margin: 0 25px;
+    &.right {
+      order: -1;
+    }
+  }
+}
+
+</style>
---
+++ src/views/Committee.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="committee">
+    <CommitteeMember v-for="(member, index) in committee" :key="member.img" :index="index" :member="member"/>
+  </div>
+</template>
+
+<script>
+import CommitteeMember from '@/components/CommitteeMember';
+
+export default {
+  components: {
+    CommitteeMember
+  },
+  data () {
+    return {
+      committee: [
+        {
+          'name': 'Jon Chia',
+          'title': 'President',
+          'img': 'jon'
+        },
+        {
+          'name': 'Dan Shields',
+          'title': 'Secretary, Live Events Officer and Tech Officer',
+          'img': 'dan'
+        },
+        {
+          'name': 'John Banks',
+          'title': 'Treasurer, Rainbow 6 Siege Rep',
+          'img': 'john'
+        },
+        {
+          'name': 'Barbora "Barb" Salcman',
+          'title': 'Lead Game Rep',
+          'img': 'barb'
+        },
+        {
+          'name': 'Josh "Fill" Fillingham',
+          'title': 'Social Secretary and Broadcast Officer',
+          'img': 'fill'
+        },
+        {
+          'name': 'James "Stallzy" Lara-Burston',
+          'title': 'Media Officer',
+          'img': 'stallzy'
+        },
+        {
+          'name': 'Devon Salopek',
+          'title': 'CS:GO Rep',
+          'img': 'devon'
+        },
+        {
+          'name': 'Jingyang "Carl" Zhong',
+          'title': 'Overwatch Rep',
+          'img': 'carl'
+        },
+        {
+          'name': 'Aiden Jenkins',
+          'title': 'Battle Royale Rep',
+          'img': 'aiden'
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
