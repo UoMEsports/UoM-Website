25e4384df5abf4d0d03e7dfd4048d8672624ca85
Dan Shields <d.shieldsuk@gmail.com>
2019-09-01 01:49:57 +0100

    fix: header sizing on all resolutions (hopefully)

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="header">
-    <img class="banner" src="@/assets/banner.jpg">
-    <img class="logo" src="@/assets/logo.png">
-    <div class="text uom-font">
-      <h2>University of Manchester</h2>
-      <h1>Esports Society</h1>
+    <div class="banner-container">
+      <img class="banner" src="@/assets/banner.jpg">
     </div>
+    <img class="header-logo" src="@/assets/header-logo.png">
   </div>
 </template>
 
@@ -18,60 +16,45 @@ export default {
 <style lang="scss" scoped>
 .header {
   position: relative;
-  height: 650px;
   width: 100%;
   overflow: hidden;
-  color: white;
+  z-index: -101;
+  margin-top: 113px;
+
+  @media only screen and (max-width: 1000px) {
+    margin-top: 0;
+  }
 
   * {
     user-select: none;
   }
-}
-.banner {
-  position: absolute;
-  left: 0;
-  top: 0;
-  z-index: -50;
-  height: 100%;
-}
 
-.logo {
-  position: absolute;
-  width: 500px;
+  .banner-container {
+    background-color: #321545;
+    overflow: hidden;
+    z-index: -100;
+    max-height: 50vh;
 
-  right: 5%;
-  top: 5%;
-}
+    .banner {
+      display: block;
+      z-index: -50;
+      position: static;
+      width: 100%;
+      min-height: 100%;
 
-.text {
-  position: absolute;
-  width: 500px;
-  right: 5%;
-  bottom: 0;
-
-  color: white;
-
-  h2 {
-    font-size: 42px;
-    margin-bottom: 0;
+      @media only screen and (max-width: 1000px) {
+        width: 120%;
+        margin-left: -20%;
+      }
+    }
   }
 
-  h1 {
-    font-size: 68px;
-    margin-top: -15px;
-    margin-bottom: 20px;
+  .header-logo {
+    position: absolute;
+    height: 90%;
+    top: 5%;
+    right: 5%;
   }
 }
 
-@media only screen and (max-width: 1720px) {
-  .banner {
-    left: auto;
-    right: -1334px;
-  }
-}
-@media only screen and (min-width: 1721px) {
-  .header {
-    height: calc(650px + 15vw - (0.15 * 1720px));
-  }
-}
 </style>
