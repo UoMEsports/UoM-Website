be7558434babb3d1d8a7afe7603bd5d692e14585
Dan Shields <d.shieldsuk@gmail.com>
2019-09-27 00:00:15 +0100

    fix: update heading styling

--- src/components/Heading.vue
+++ src/components/Heading.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="heading-container uom-font">
+  <div class="heading-container uom-font" :class="{centered: align === 'center'}">
     <div class="heading">
       <h1 class="text"><slot></slot></h1>
       <hr class="underline">
@@ -9,6 +9,9 @@
 
 <script>
 export default {
+  props: [
+    'align'
+  ]
 };
 </script>
 
@@ -18,25 +21,32 @@ export default {
   text-align: left;
   padding-bottom: 35px;
 
-  @media only screen and (min-width: 1001px) {
-    display: none;
+  &.centered {
+    text-align: center;
+
+    .heading .underline {
+      width: 100%;
+    }
+  }
+
+  .heading {
+    display: inline-block;
+
+    .text {
+      color: black;
+      font-size: 50px;
+      text-align: left;
+      margin-bottom: 0;
+
+      @media only screen and (max-width: 1000px) {
+        font-size: 35px;
+      }
+    }
+
+    .underline {
+      border-top: 3px solid $dark;
+      width: calc(100% + 80px);
+    }
   }
 }
-
-.heading {
-  display: inline-block;
-}
-
-.text {
-  color: black;
-  font-size: 50px;
-  text-align: left;
-  margin-bottom: 0;
-}
-
-.underline {
-  border-top: 3px solid $dark;
-  width: calc(100% + 80px);
-}
-
 </style>
