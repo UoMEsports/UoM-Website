f78a23a4e6eef146c81b05e21c5684479899a1bd
Dan Shields <d.shieldsuk@gmail.com>
2019-09-26 23:58:09 +0100

    fix: update event info

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -5,14 +5,14 @@
         <a href="https://kotn.uomesports.co.uk" target="_blank"><img src="@/assets/kotn.jpg"></a>
       </template>
 
-      King of the North is UoM Esports' biggest yearly event, with previous years playing host to up to 350 players and attracting hundreds of thousands of views on Twitch.
+      King of the North is UoM Esports' biggest yearly event, with previous years playing host to up to 350 players, and attracting 600+ spectators in the audience, as well as hundreds of thousands of views on Twitch.
     </Event>
     <Event>
       <template v-slot:image>
         <a href="https://hiveaid.uomesports.co.uk" target="_blank"><img src="@/assets/hiveaid.jpg"></a>
       </template>
 
-      HiveAid was only added to our event calendar last year, but has already been a big success. The 2 day charity livestream beat our £500 target and was in aid of SpecialEffect. The main attraction is the all-star lineups from teams across the UK University esports scene.
+      HiveAid was only added to our event calendar last year, but has already been a big success. The 2 day charity livestream was in aid of SpecialEffect and we beat our £500 target. The main attraction of the event is the all-star lineups from teams across the UK University esports scene.
     </Event>
   </div>
 </template>
