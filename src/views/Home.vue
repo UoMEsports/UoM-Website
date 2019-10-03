fda47d56f195db16fcfae8b84b49c86fcacda4b6
Dan Shields <d.shieldsuk@gmail.com>
2019-10-03 16:48:19 +0100

    fix: update home page styling

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="home">
-    <div>
+    <div class="description">
       <p>UoM Esports Society aims to promote grassroot esports in the university scene. We do this by entering our members into student only leagues such as the NUEL and NSE. We also host regular socials from casual meetups or viewing parties to bowling and other more organised events.</p>
-      <p>To join our society, just click the "Join this group" button at the top of this page. Be sure to check out our Facebook group where we post our socials. As well as checking out our Discord server, discord.gg/UoMEsports, where we host tryouts and game nights.</p>
       <p>We run tryouts for as many teams as we can create, so it doesn't matter if you're a high skilled competitive player or a more relaxed casual player there will be a team for you.</p>
     </div>
+    <Heading class="heading">Games We Support</Heading>
     <div class="games">
       <div class="column">
         <div class="game">
@@ -12,7 +12,7 @@
           <h1>League of Legends</h1>
         </div>
         <div class="game">
-          <img src="@/assets/games/overwatch.svg">
+          <img src="@/assets/games/overwatch.png">
           <h1>Overwatch</h1>
         </div>
         <div class="game">
@@ -55,7 +55,11 @@
 </template>
 
 <script>
+import Heading from '@/components/Heading.vue';
 export default {
+  components: {
+    Heading
+  }
 };
 </script>
 
@@ -65,24 +69,46 @@ export default {
   max-width: 100%;
   margin: auto;
   font-size: 20px;
+
+  .heading {
+    margin-bottom: -20px;
+  }
 }
+
+.description {
+  text-align: left;
+}
+
 .games {
   display: flex;
   flex-wrap: wrap;
   justify-content: space-around;
+
+  margin-bottom: 50px;
+
   .column {
     width: 450px;
     max-width: 100%;
+
     .game {
       h1 {
-      font-size: 30px;
-      text-align: left;
-      line-height: 50px;
+        font-size: 30px;
+        text-align: left;
+        line-height: 50px;
+
+        @media only screen and (max-width: 850px) {
+          line-height: 30px;
+        }
       }
+
       img {
         float: left;
         height: 50px;
         margin-right: 20px;
+
+        @media only screen and (max-width: 850px) {
+          height: 30px;
+        }
       }
     }
   }
