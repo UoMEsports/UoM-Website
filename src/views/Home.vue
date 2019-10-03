50fdfe03fe636ff7d73d39e1c472665a6ea20cb2
Dan Shields <d.shieldsuk@gmail.com>
2019-10-03 02:39:27 +0100

    feat: add home page info

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="home">
+    <p>The Esports Society aims to promote grassroot esports in the university scene. We do this by entering our members into student only leagues such as the NUEL and NSE. We also host regular socials from casual meetups or viewing parties to bowling and other more organised events.</p>
+    <p>To join our society, just click the "Join this group" button at the top of this page. Be sure to check out our Facebook group where we post our socials. As well as checking out our Discord server, discord.gg/UoMEsports, where we host tryouts and game nights.</p>
+    <p>We run tryouts for as many teams as we can create, so it doesn't matter if you're a high skilled competitive player or a more relaxed casual player there will be a team for you.</p>
   </div>
 </template>
 
 <script>
 export default {
-
 };
 </script>
+
+<style lang="scss" scoped>
+.home {
+  width: 1000px;
+  margin: auto;
+}
+</style>
